a23cfd8f6614ba566726dfec9e0567e5a5b6c1c6
millenarian <kvk0x@protonmail.com>
2022-04-10 17:16:54 -0700

    do impl of decreasing unstaked deposits

--- contracts/AtlasMineStakerUpgradeable.sol
+++ contracts/AtlasMineStakerUpgradeable.sol
@@ -284,6 +284,14 @@ contract AtlasMineStakerUpgradeable is
             _unstakeToTarget(payout - _totalUsableMagic());
         }
 
+        // Decrement unstakedDeposits based on how much we are withdrawing
+        // If we are withdrawing more than is currently unstaked, set it to 0
+        if (_amount >= unstakedDeposits) {
+            unstakedDeposits = 0;
+        } else {
+            unstakedDeposits -= _amount;
+        }
+
         emit UserWithdraw(msg.sender, depositId, _amount, reward);
     }
 
@@ -597,6 +605,7 @@ contract AtlasMineStakerUpgradeable is
         IERC1155Upgradeable(treasureAddr).setApprovalForAll(address(mine), false);
 
         address legionAddr = mine.legion();
+
         IERC721Upgradeable(legionAddr).setApprovalForAll(address(mine), false);
     }
 
