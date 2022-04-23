e5224c9b9eb8b735c57f104be4d4ef6ce6d0d8af
Aeto 0x <0xaeto@gmail.com>
2022-04-22 21:13:35 -0700

    refactor(_withdraw): require withdraws to be > 0

--- contracts/AtlasMineStakerUpgradeable.sol
+++ contracts/AtlasMineStakerUpgradeable.sol
@@ -261,6 +261,7 @@ contract AtlasMineStakerUpgradeable is
         uint256 depositId,
         uint256 _amount
     ) internal returns (uint256 payout) {
+        require(_amount > 0, "Withdraw amount 0");
         if (_amount > s.amount) {
             _amount = s.amount;
         }
